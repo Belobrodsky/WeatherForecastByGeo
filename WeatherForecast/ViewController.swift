735f33b663ef6baef5001dd674f2003db410cd7e
Vladimir Belobrodsky <belobrodsky@yandex.ru>
2022-02-06 20:49:29 +0300

    print Coord in console

--- WeatherForecast/ViewController.swift
+++ WeatherForecast/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startLocationManager();
     }
 
+    func startLocationManager()
+    {
+        //послать на устройство запрос на использования геопозиции при использовании приложения
+        locationManager.requestWhenInUseAuthorization()
+        
+        //Если на устройстве включены службы геолокации
+        if CLLocationManager.locationServicesEnabled() {
+            
+            //передать действие тому, кто будет обрабатывать при изменении координат, для этого нужно этот класс подписать на протокол  CLLocationManagerDelegate
+            locationManager.delegate = self
+            
+            // точность 100 метров
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            
+            locationManager.pausesLocationUpdatesAutomatically = false
+            
+            //запускаем слежку за локацией и при изменении будет запускаться метод из протокола CLLocationManagerDelegate
+            locationManager.startUpdatingLocation()
+            
+        }
+              
+    }
 
 }
-
+// подпишем наш класс на протокол и в этом месте код будет отвечать за обработку изменений координат при перемещении
+extension ViewController: CLLocationManagerDelegate{
+    // тут нужно создать вункцию которая будет выполняться при изменении кординат для этого нужно начать писать "didUp"
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let lastLocation = locations.last {
+            print(lastLocation.coordinate.latitude,  lastLocation.coordinate.longitude)
+            
+        }
+    }
+    
+}
